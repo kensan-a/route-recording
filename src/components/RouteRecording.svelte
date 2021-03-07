5770bdeebb6514dfac39867163b78b70fd6219c1
kensan-a <44194688+kensan-a@users.noreply.github.com>
2021-03-07 16:03:07 +0900

    2103071603

--- src/components/RouteRecording.svelte
+++ src/components/RouteRecording.svelte
@@ -3,9 +3,8 @@
   import mapboxgl from "mapbox-gl";
 
   // import Map from "../components/Map.svelte";
-  // import { cmanPosWatch } from "./common/getLocation";
 
-  const { Map, NavigationControl, ScaleControl } = mapboxgl;
+  const { Map, NavigationControl, ScaleControl, GeolocateControl } = mapboxgl;
   let map;
   let container;
 
@@ -17,8 +16,94 @@
       container,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [139.7644081, 35.680043],
-      zoom: 10
+      zoom: 18
     });
+
+    map.addControl(new NavigationControl());
+
+    map.addControl(
+      new ScaleControl({
+        maxWidth: 200,
+        unit: "metric"
+      }),
+      "bottom-right"
+    );
+
+    map.addControl(
+      new GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        trackUserLocation: false
+      })
+    );
+
+    map.on("moveend", e => {
+      console.log(map.getCenter());
+      console.log(map.getZoom());
+    });
+  });
+
+  const flyToCurrentPosition = () => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    const success = pos => {
+      const { latitude, longitude, accuracy } = pos.coords;
+
+      console.log("Your current position is:");
+      console.log(`Latitude : ${latitude}`);
+      console.log(`Longitude: ${longitude}`);
+      console.log(`More or less ${accuracy} meters.`);
+
+      map.flyTo({
+        center: [longitude, latitude],
+        zoom: map.getZoom()
+      });
+    };
+
+    const error = err => {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  };
+</script>
+
+<svelte:head>
+  <link href='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css' rel='stylesheet' />
+</svelte:head>
+
+<style>
+  .wapper {
+    display: flex;
+  }
+
+  .map {
+    width: 100%;
+    height: 90%;
+  }
+</style>
+
+<a href="#!" target="_self" on:click={flyToCurrentPosition}>
+  現在位置へ
+</a>
+
+<div class="map" bind:this={container}></div>
+<!-- <Map /> -->
+
+  <!-- <script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRo134Nk1ubkPS5gm6mdyQ64gCVUhSIuc&libraries=places"
+  ></script> -->
+
+  <!-- const startMap = () => {
+    cmanPosWatch(window.google.maps);
+  }; -->
+
+  <!-- // import { cmanPosWatch } from "./common/getLocation";
 
     // const map = new Map({
     //   container: "watMap",
@@ -45,74 +130,4 @@
     //     center: [139.7644081, 35.680043],
     //     zoom: 10
     //   }
-    // });
-
-    map.addControl(new NavigationControl());
-
-    map.addControl(
-      new ScaleControl({
-        maxWidth: 200,
-        unit: "metric"
-      }),
-      "bottom-right"
-    );
-
-    map.on("moveend", e => {
-      console.log(map.getCenter());
-      console.log(map.getZoom());
-    });
-  });
-
-  const startMap = () => {
-    // cmanPosWatch(window.google.maps);
-
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
-
-    function success(pos) {
-      var crd = pos.coords;
-
-      console.log("Your current position is:");
-      console.log(`Latitude : ${crd.latitude}`);
-      console.log(`Longitude: ${crd.longitude}`);
-      console.log(`More or less ${crd.accuracy} meters.`);
-
-      map.flyTo({ center: [crd.longitude, crd.latitude] });
-    }
-
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  };
-</script>
-
-<svelte:head>
-  <!-- <script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRo134Nk1ubkPS5gm6mdyQ64gCVUhSIuc&libraries=places"
-  ></script> -->
-
-  <link href='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css' rel='stylesheet' />
-</svelte:head>
-
-<style>
-  .wapper {
-    display: flex;
-  }
-
-  .map {
-    width: 100%;
-    height: 90%;
-  }
-</style>
-
-<a href="#!" target="_self" on:click={startMap}>
-  現在位置へ
-</a>
-
-<div class="map" bind:this={container}></div>
-<!-- <Map /> -->
+    // }); -->
